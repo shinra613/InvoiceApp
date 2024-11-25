b26e5d22d95c0f2c6e6f411a5d895613ccd2c9e9
Akhil Venkatesh Pillai <akhiltechwiz@gmail.com>
2024-11-25 14:26:52 +0530

    revamp

--- src/config/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    port: process.env.PORT || 3000,
-    databaseUrl: process.env.DATABASE_URL || 'sqlite:invoice.db'
-  };
-  
--- src/middlewares/logger.js
+++
@@ -1,8 +0,0 @@
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    console.log(req.body);
-    next();
-  };
-  
-  module.exports = logger;
-  
